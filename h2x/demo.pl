84f7a3cae4c23cb708a6c1e80483743bef029aab
Sitaram Iyer <sitaram@gmail.com>
2020-03-15 18:59:35 -0700

    demo

--- h2x/demo.pl
+++ h2x/demo.pl
@@ -119,6 +119,10 @@ sub printonecase($$) {
           color: #1a0dab;
         }
       }
+      .nextlink {
+        float: right;
+        margin-right: 50%;
+      }
       .expando {
         float: right;
         margin-right: 50%;
@@ -199,6 +203,9 @@ sub printonecase($$) {
           margin: 4px -16px;
           padding: 0 16px;
         }
+        .nextlink {
+          margin-right: 0;
+        }
         .expando {
           margin-right: 0;
         }
@@ -259,14 +266,17 @@ sub printonecase($$) {
     $stuff .= "<div class=\"bar\">";
     foreach my $child (@{$children{$path}}) {
       my $target = $file{$path . " > " . $child};
-
       my $svg = '<svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M20.49 19l-5.73-5.73C15.53 12.2 16 10.91 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.41 0 2.7-.47 3.77-1.24L19 20.49 20.49 19zM5 9.5C5 7.01 7.01 5 9.5 5S14 7.01 14 9.5 11.99 14 9.5 14 5 11.99 5 9.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>';
-      
       my $url = $target ? "$target.html" : search($query{$path . " > " . $child});
       $stuff .= "<a href=\"$url\"><div class=\"chip\">$svg$child</div></a>";
-
       print $child, " / ", $query{$path . " > " . $child};
       print "---> ".$target if $target;
+    }
+    if (scalar @{$children{$path}} == 0 && $p < scalar @paths - 1) {  # endpoint - add a next link
+      my $n = $paths[$p+1];
+      my $t = $paths[$p+1];
+      $t =~ s/^.*> *//;
+      $stuff .= "<a class=\"bc nextlink\" href=\"$file{$n}.html\">Next topic: $t &#xBB;</a>";
     }
     print "";
     $stuff .= "</div>";
